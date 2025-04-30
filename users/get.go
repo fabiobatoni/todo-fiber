60025dc39bf083f36f8de1006a1a3292fe50bc44
Fabio Robledo Batoni de Godoy <fabio_batoni@hotmail.com>
2025-04-30 15:40:18 -0300

    create routes get users

--- users/get.go
+++ users/get.go
@@ -18,3 +18,15 @@ func getAll(c *fiber.Ctx) error {
 
 	return c.JSON(documents)
 }
+
+func getByID(c *fiber.Ctx) error {
+	var document User
+
+	err := db.FindByID("users", c.Params("id"), &document)
+
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(err.Error())
+	}
+
+	return c.JSON(document)
+}
